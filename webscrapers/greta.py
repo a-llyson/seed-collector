b3bf28e0bb23a3b0248e2c266a2813ba3c5c7901
a-llyson <67112226+a-llyson@users.noreply.github.com>
2022-08-06 00:18:43 -0400

    Fixed starting url

--- webscrapers/greta.py
+++ webscrapers/greta.py
@@ -2,12 +2,14 @@ import scrapy
 
 class gretaSpider(scrapy.Spider):
     name = "greta"
-    start_urls = "https://www.seeds-organic.com/collections/all"
+    start_urls = ["https://www.seeds-organic.com/collections/all"]
 
     # either get seed names from /all page or from each individual product page?
     def parse(self, response):
         item_num = 1
+        print("ere")
         for seed_name in response.xpath(f'/html/body/main/div[2]/div/div[2]/div/ul/li[{item_num}]/div/div/div[2]/div[1]/h3/a'):
+            
             seed = seed_name.css('::text').get() # returns '\n        seed_name\n      '
             seed = seed.replace('\n', '') # strips \n from name, could also strip -organic if wanted)
             seed = seed.strip() # strips whitespace
@@ -16,8 +18,8 @@ class gretaSpider(scrapy.Spider):
                 'seed':  seed
             }
 
-            product_page_css = seed_name.css('') # a tags get href selected automatically
-            yield from response.follow_all(product_page_css, self.productParse)
+            # product_page_css = seed_name.css('') # a tags get href selected automatically
+            # yield from response.follow_all(product_page_css, self.productParse)
             
             # or if new page is loaded, then set it back to 0
             if item_num > 24:
