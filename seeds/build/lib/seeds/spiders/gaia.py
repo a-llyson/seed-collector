dbad8f18dd741b30978ed817206af6b9b0d3141b
a-llyson <67112226+a-llyson@users.noreply.github.com>
2022-08-06 20:44:53 -0400

    Shub build

---
+++ seeds/build/lib/seeds/spiders/gaia.py
@@ -0,0 +1,71 @@
+import scrapy
+
+class gaiaSpider(scrapy.Spider):
+    name = "gaia"
+
+    start_urls = ["https://gaiaorganics.ca/shop/"]
+
+    # either get seed names from each individual product page
+    def parse(self, response):
+        item_num = 1
+        # all seeds (24) from one page 
+        while item_num <= 25: 
+            xpath = f'/html/body/div[2]/section/div/div[1]/ul/li[{item_num}]/a'
+            if not (response.xpath(xpath)):
+                xpath = f'/html/body/div[1]/section/div/div[1]/ul/li[{item_num}]/a'
+
+            seed=response.xpath(xpath)[0]
+
+            product_page_css = seed.css('a::attr(href)') # a tags get href selected automatically
+
+            yield from response.follow_all(product_page_css, self.product_parse)
+                
+            item_num += 1
+
+
+        # scrapy throws error for page 72 but everything on page 72 is processed
+        # goes to next page 
+        next_arrow = response.css('.next::attr(href)')
+        if next_arrow:
+            yield from response.follow_all(next_arrow, self.parse)
+
+
+    def product_parse(self, response):
+        # gets seed name
+        seed = response.css('.product_title::text').get() 
+
+        # gets price 
+        price = response.css('p.price span bdi::text').get()
+
+        # gets seed quantity (smallest)
+        if response.css('option::text'):
+            seed_qty = response.css('option::text')[1].get().lower()
+        else:
+            seed_qty = "OOS"
+
+        # If seed quantity is not given
+        # if "seed" not in seed_qty and " g" not in seed_qty and "OOS" not in seed_qty and "kg" not in seed_qty and "bulb" not in seed_qty and "g" not in seed_qty:
+        #     seed_qty = "N/A"
+
+        # replace unicode with ' and removes whitespace
+        seed = seed.replace('\u2019', '\'').strip() 
+
+        if "kg" in seed_qty:
+            seed_qty = seed_qty[:5]
+        elif "bulb" in seed_qty:
+            seed_qty = seed_qty[4:]
+        elif "g" in seed_qty:
+            seed_qty = seed_qty[:5]
+        else:
+            seed_qty = seed_qty[:11]
+
+        # remove \n, ~, $, "seeds" and whitespace
+        seed_qty = seed_qty.replace('seeds', '').replace('-', '').replace('$', '').strip()
+
+        # to filter out the live plants for pickup at their ottawa location
+        if price:
+            yield {
+                'seed':  seed,
+                'price': price,
+                'qty': seed_qty,
+            }
---
+++ seeds/build/lib/seeds/spiders/greta.py
@@ -0,0 +1,55 @@
+import scrapy
+
+class gretaSpider(scrapy.Spider):
+    name = "greta"
+    
+    start_urls = ["https://www.seeds-organic.com/collections/all"]
+
+    # either get seed names from each individual product page
+    def parse(self, response):
+        item_num = 1
+
+        # all seeds (24) from one page 
+        while item_num <= 24:
+            for seed in response.xpath(f'/html/body/main/div[2]/div/div[2]/div/ul/li[{item_num}]/div/div/div[2]/div[1]/h3/a'):
+                
+                product_page_css = seed.css('a::attr(href)') # a tags get href selected automatically
+
+                yield from response.follow_all(product_page_css, self.product_parse)
+                
+            item_num += 1
+        
+        # goes to next page 
+        next_arrow = response.css('.pagination__item--prev::attr(href)')
+        if next_arrow:
+            yield from response.follow_all(next_arrow, self.parse)
+
+
+    def product_parse(self, response):
+        # gets seed name
+        seed = response.css('h1.product__title::text').get() 
+
+        # gets price 
+        price = response.css('span.price-item--regular::text').get()
+
+        # gets seed quantity (the first label)
+        seed_qty = response.css('label::text').get()
+
+        # If seed quantity is not given
+        if "seeds" not in seed_qty and " g" not in seed_qty:
+            seed_qty = "N/A"
+
+        # replace unicode with ' and removes "organic" and whitespace
+        seed = seed.replace('\u2019', '\'').replace('- Organic', '').replace('-Organic', '').strip() 
+
+        # remove $, CAD and whitespace
+        price = price.replace('$', '').replace('CAD', '').strip()
+        
+        # remove \n, ~, "seeds" and whitespace
+        seed_qty = seed_qty.replace('\n', '').replace('~', '').replace('seeds', '').strip()
+
+        yield {
+            'seed':  seed,
+            'price': price,
+            'qty': seed_qty,
+        }
