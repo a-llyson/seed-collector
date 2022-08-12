e36fa5bff510c965b86eb4db47e6b1ff28e651d4
a-llyson <67112226+a-llyson@users.noreply.github.com>
2022-08-11 23:33:53 -0400

    MongoDB debugging

--- seeds/seeds/items.py
+++ seeds/seeds/items.py
@@ -9,4 +9,7 @@ import scrapy
 class SeedsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    seed = scrapy.Field()
+    price = scrapy.Field()
+    qty = scrapy.Field()
--- seeds/seeds/pipeline.py
+++ seeds/seeds/pipeline.py
@@ -1,6 +1,6 @@
 import pymongo
 import logging
-
+from itemadapter import ItemAdapter
 
 class MongoPipeline():
 
@@ -9,29 +9,32 @@ class MongoPipeline():
     def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
-        logging.info(mongo_uri)
-        logging.info(mongo_db)
 
     @classmethod
     def from_crawler(cls, crawler):
-        mongo_uri=crawler.settings.get('MONGO_URI')
-        mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
-        logging.INFO(mongo_uri)
-        logging.INFO(mongo_db)
         return cls(
             mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
         )
 
     def open_spider(self, spider):
         self.client = pymongo.MongoClient(self.mongo_uri)
+        logging.info(f"login? {self.client}")
         self.db = self.client[self.mongo_db]
+        logging.info(f"db? {self.db}")
+        print(self.db.command("ismaster"))
     
     def close_spider(self, spider):
         self.client.close()
 
     def process_item(self, item, spider):
-        self.db[self.collection_name].insert_one(dict(item))
+        logging.info(f"in progress: {item}")
+        logging.info(f"db: {self.db}")
+        logging.info(f"collection: {self.collection_name}")
+        logging.info(f"together: {self.db[self.collection_name]}")
+        logging.info(f"item inserting: {ItemAdapter(item).asdict()}")
+        res = self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())
+        
         logging.info("processed item")
-        logging.info(item)
+        print(res)
         return item
