cae5d73d9552d685098f6dc8f52b3746ddf4f8d2
a-llyson <67112226+a-llyson@users.noreply.github.com>
2022-08-13 14:37:25 -0400

    Notion integration

--- seeds/seeds/pipeline.py
+++ seeds/seeds/pipeline.py
@@ -4,7 +4,7 @@ from itemadapter import ItemAdapter
 
 class MongoPipeline():
 
-    collection_name = 'greta'
+    collection_name = 'osc'
 
     def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
@@ -19,29 +19,12 @@ class MongoPipeline():
 
     def open_spider(self, spider):
         self.client = pymongo.MongoClient(self.mongo_uri)
-        logging.info(f"login? {self.client}")
         self.db = self.client[self.mongo_db]
-        logging.info(f"db? {self.db}")
-        print(self.db.command("ismaster"))
-        try:
-            self.client.admin.command('ping')
-        except ConnectionFailure:
-            print("Server not available")
 
-
-    
     def close_spider(self, spider):
         self.client.close()
 
     def process_item(self, item, spider):
-        logging.info(f"in progress: {item}")
-        logging.info(f"db: {self.db}")
-        logging.info(f"collection: {self.collection_name}")
-        logging.info(f"together: {self.db[self.collection_name]}")
-        logging.info(f"item inserting: {ItemAdapter(item).asdict()}")
-        res = self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())
-        
+        self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())   
         logging.info("processed item")
-        print(res)
-        print(self.db[self.collection_name].find_one())
         return item
